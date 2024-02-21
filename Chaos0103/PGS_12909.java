012766a59db15c01589aae76b542975893c0c3e4
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-02-21 17:50:09 +0900

    올바른 괄호 / 5분

---
+++ Chaos0103/PGS_12909.java
@@ -0,0 +1,23 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        char[] arr = s.toCharArray();
+        for (char c : arr) {
+            if (c == '(') {
+                stack.add(c);
+                continue;
+            }
+
+            if (stack.isEmpty()) {
+                return false;
+            }
+
+            stack.pop();
+        }
+
+        return stack.isEmpty();
+    }
+}
